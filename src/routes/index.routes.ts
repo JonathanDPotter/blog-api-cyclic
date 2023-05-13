8140b821f2eb49e3d871b7230c3b7de218714bef
Jonathan Potter <jonathan.d.potter@outlook.com>
2023-05-13 08:52:05 -0400

    Changed index route response

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,9 +1,10 @@
 import { Request, Response, Router } from "express";
+import path from "path";
 
 const router = Router();
 
 router.get("/", (_req: Request, res: Response) => {
-  res.status(200).json("working");
+  res.status(200).sendFile(path.join(__dirname, "../static/index.html"));
 });
 
 export default router;
--- src/utils/createServer.ts
+++ src/utils/createServer.ts
@@ -3,16 +3,18 @@ import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import indexRoutes from "../routes/index.routes";
+import path from "path";
 
 const createServer = () => {
   const server = express();
 
+  server.use(express.static(path.join(__dirname, "../static")));
   server.use(express.json());
   server.use(cors({ origin: "*" }));
   server.use(morgan("dev"));
   server.use(helmet());
   server.use(indexRoutes);
   return server;
-}
+};
 
 export default createServer;
