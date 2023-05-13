89549ce2dbee0f72c0ba75ac237d81a2d0c518f6
Jonathan Potter <jonathan.d.potter@outlook.com>
2023-05-13 09:33:09 -0400

    updated static folder path

--- src/utils/createServer.ts
+++ src/utils/createServer.ts
@@ -4,11 +4,18 @@ import helmet from "helmet";
 import morgan from "morgan";
 import indexRoutes from "../routes/index.routes";
 import path from "path";
+import config from "../config";
+
+const { env } = config.server;
 
 const createServer = () => {
   const server = express();
 
-  server.use(express.static(path.join(__dirname, "../static")));
+  server.use(
+    express.static(
+      path.join(__dirname, `../${env === "development" ? "" : "src/"}static`)
+    )
+  );
   server.use(express.json());
   server.use(cors({ origin: "*" }));
   server.use(morgan("dev"));
